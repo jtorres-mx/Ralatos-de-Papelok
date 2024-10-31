3b2b66ac5fd432f445d92a0fb94f84aa782cb512
Pablo <Pablo@Pablos-MBP.station>
2024-10-31 09:21:09 +0000

    plants shop ecommerce

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Plants Shop</h1>
+            <p>The best place to buy your plants</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
